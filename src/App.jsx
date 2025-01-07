3b520f58ee1271449bbe87a84a974b81a45d9b47
Juliana Gagliano <julianagagliano1@gmail.com>
2025-01-07 10:45:21 -0300

    draft: component + states

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 function App() {
 
-  const message = 'Olá, Mundo'
+  const [message, setMessage] = useState("Hello, Ju!")
 
   return (
     <div>
       <h1>{message}</h1>
+      <button onClick={() => {
+        setMessage("The button was clicked")
+      }}>Change message</button>
     </div>
   )
 }
