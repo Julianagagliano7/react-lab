f5a0cff6f331699401d177c9ca0b616aa00ff399
Juliana Gagliano <julianagagliano1@gmail.com>
2025-01-11 20:57:57 -0300

    feat: AddTask component

--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,7 +1,39 @@
-function AddTasks() {
+import { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+
+function AddTasks({ onAddTaskSubmit }) {
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
     return (
-        <div>
-            <h1></h1>
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+            <input
+                type="text"
+                placeholder="Digite o título da tarefa"
+                className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+            />
+            <input
+                type="text"
+                placeholder="Digite a descrição da tarefa"
+                className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+            />
+            <button
+                className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+                onClick={() => {
+                    if (!title.trim() || !description.trim()) {
+                        return toast.error("Título e descrição devem ser preenchidos")
+                    }
+                    onAddTaskSubmit(title, description);
+                    setTitle("")
+                    setDescription("")
+                }}
+            >Adicionar</button>
+            <ToastContainer />
         </div>
     )
 }
