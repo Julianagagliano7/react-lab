c41fd73d0a9acdc9479d1f18e06f49b11206a7af
Juliana Gagliano <julianagagliano1@gmail.com>
2025-02-11 11:45:13 -0300

    fix: add icon when task is done

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -14,13 +14,14 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
     }
 
     return (
-        
+
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             {tasks.map((task) => (
                 <li key={task.id} className="flex gap-2">
                     <button
                         onClick={() => onTaskClick(task.id)}
-                        className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && 'line-through'} `}>
+                        className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${task.isCompleted && 'line-through'} `}>
+                        {task.isCompleted && <CheckIcon />}
                         {task.title}
                     </button>
                     <Button
